77f041ad08af928c65cb139a3cae630242dde8d9
###jeffrixxxon <99013851+jeffrixxxon@users.noreply.github.com>
2022-11-19 14:14:29 +0300

    Edit file

--- auth_data.py
+++ auth_data.py
@@ -1,1 +1,6 @@
 token = '5643913701:AAGxM3YT69pUtyODwC0oA7jNqXcu1QWc6Rs'
+
+category_dict = {'Smartphones 📱': '205', 'Tablets 📲': '195', 'Laptops 💻': '118', 'Televisions 📺': '1', 'Refrigerators 🚪': '159', 'Washing machines 🧼': '89'}
+category_list = ['Smartphones 📱', 'Tablets 📲', 'Laptops 💻', 'Televisions 📺', 'Refrigerators 🚪', 'Washing machines 🧼']
+
+generate_numbers = [str(i) for i in range(10, 100, 10)]
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,11 +1,12 @@
 import json
 import logging
 import time
+import asyncio
 
 from main_pagination import get_data
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
-from auth_data import token
+from auth_data import token, category_dict, category_list, generate_numbers
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
@@ -21,14 +22,9 @@ async def start(message: types.Message):
 	await message.answer('Choose a parsing mod 🤌🏻', reply_markup=keyboard)
 
 
-@dp.message_handler(Text(equals=['Stop 🔚']))
-async def stop(message: types.Message):
-	await start(message)
-
-
 @dp.message_handler(Text(equals=['Go parsing! 👾', 'Restart 🔙']))
 async def category(message: types.Message):
-	start_buttons = ['Smartphones 📱', 'Tablets 📲', 'Laptops 💻', 'Televisions 📺', 'Refrigerators 🚪', 'Washing machines 🧼']
+	start_buttons = category_list
 	keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 	keyboard.add(*start_buttons)
 
@@ -36,54 +32,63 @@ async def category(message: types.Message):
 
 
 @dp.message_handler(Text(equals=['View sale 😏']))
-@dp.message_handler(Text(equals=['Smartphones 📱', 'Tablets 📲', 'Laptops 💻', 'Televisions 📺', 'Refrigerators 🚪', 'Washing machines 🧼']))
+@dp.message_handler(Text(equals=category_list))
 async def parsing(message: types.Message):
-	data_category = {'Smartphones 📱': '205', 'Tablets 📲': '195', 'Laptops 💻': '118', 'Televisions 📺': '1', 'Refrigerators 🚪': '159', 'Washing machines 🧼': '89'}
-	start_buttons = [str(i) for i in range(10, 100, 10)]
+	data_category = category_dict
+	start_buttons = generate_numbers
 	keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 	keyboard.add(*start_buttons)
-	await message.answer('Please waiting... parsing procedure...')
+
 	if message.text != 'View sale 😏':
+		await message.answer('Please waiting... parsing procedure...')
+		print(f'[INFO] {message.from_user.first_name} connected!')
 		get_data(data_category[message.text])
 		print('[!]Parsing complete')
-		print(f'[INFO] {message.from_user.first_name} parsit!')
-		await message.answer('Enter the tracked discount 💲💲💲', reply_markup=keyboard, )
 		await message.answer('Complete ✅')
+		time.sleep(1)
+		await message.answer('Enter the tracked discount 💲💲💲', reply_markup=keyboard, )
 	else:
-		await message.answer('Enter the tracked discount 💲💲💲', reply_markup=keyboard, )
 		await message.answer('Complete ✅')
+		time.sleep(1)
+		await message.answer('Enter the tracked discount 💲💲💲', reply_markup=keyboard, )
 
 
-@dp.message_handler(Text(equals=[str(i) for i in range(10, 100, 10)]))
-async def search_sale(message: types.Message):
-	start_buttons = ['Restart 🔙', 'Stop 🔚', 'View sale 😏']
+@dp.message_handler(Text(equals=generate_numbers))
+async def event_handler(message: types.Message):
+	start_buttons = ['View sale 😏', 'Restart 🔙']
 	keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 	keyboard.add(*start_buttons)
 	await message.answer('Complete ✅', reply_markup=keyboard)
+	await search_sale(message)
+
+
+@dp.message_handler(Text(equals=['Restart 🔙']))
+async def search_sale(message: types.Message, num):
 	with open('data/result_prices.json') as file:
 		reader = json.load(file)
 	cnt = 0
 	for key, val in reader.items():
+
+		# elif message.text is 'Restart 🔙':
+		# 	await category(message.text)
+		# 	break
 		base_price = val.get('item_basePrice')
 		sale_price = val.get('item_salePrice')
 		card = f"Article number: {key}\n" \
 		       f"Base price: {base_price} Руб.\n" \
 		       f"Discounted price: {sale_price} Руб.\n" \
 		       f"Discount: {int(abs((sale_price / base_price) * 100 - 100))}%"
-		if message.text == 'Stop 🔚':
-			await stop(message)
-		if abs((sale_price / base_price) * 100 - 100) >= int(message.text):
+		if int(num) <= abs((sale_price / base_price) * 100 - 100) <= int(num) + 10:
 			time.sleep(2)
 			await message.answer(card)
 			cnt += 1
+		elif message.text == 'View sale 😏':
+			await parsing()
+			break
 	if cnt == 0:
 		await message.answer('No items 😟')
-	elif message.text == 'View sale 😏':
-		await parsing()
-	elif message.text == 'Restart 🔙':
-		await category(message.text)
-	elif message.text == 'Stop 🔚':
-		await stop(message.text)
+	else:
+		await message.answer(f'Number of products: {cnt}')
 
 
 def main():
