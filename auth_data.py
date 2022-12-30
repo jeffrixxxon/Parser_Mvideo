f5707f3212066a1834412d2de042b10b64321fc8
###jeffrixxxon <99013851+jeffrixxxon@users.noreply.github.com>
2022-12-30 20:33:39 +0300

    Create file

--- auth_data.py
+++ auth_data.py
@@ -1,3 +1,8 @@
 token = '5958726076:AAEKD-zpL3VMfwd8A9uoJW37icRZdhOMHRA'
 
 sales_numbers = [str(i) for i in range(10, 100, 10)]
+
+buttons_search = ['Smartphones', 'Tablets', 'Laptops', 'Televisions', 'Refrigerators', 'Washing machines', 'View sale']
+
+data_base_category = {'Smartphones': '205', 'Tablets': '195', 'Laptops': '118', 'Televisions': '1',
+	                  'Refrigerators': '159', 'Washing machines': '89'}
--- telegram_bot.py
+++ telegram_bot.py
@@ -3,18 +3,19 @@ import logging
 import os
 import time
 
-from main_pagination import get_data
-from aiogram import Bot, Dispatcher, executor, types
+
 from aiogram.dispatcher.filters import Text
 from aiogram.utils.markdown import hlink
-from auth_data import token, sales_numbers
+from aiogram import Bot, Dispatcher, executor, types
+from main_pagination import get_data
+from auth_data import token, sales_numbers, buttons_search, data_base_category
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands='start', )
+@dp.message_handler(commands='start')
 async def start(message: types.Message):
 	await message.answer(f'Hello, {message.from_user.first_name}!')
 	start_buttons = ['Go parsing!']
@@ -25,7 +26,7 @@ async def start(message: types.Message):
 
 @dp.message_handler(Text(equals=['Go parsing!', 'Back']))
 async def category(message: types.Message):
-	start_buttons = ['Smartphones', 'Tablets', 'Laptops', 'Televisions', 'Refrigerators', 'Washing machines', 'View sale']
+	start_buttons = buttons_search
 	keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 	keyboard.add(*start_buttons)
 
@@ -35,9 +36,8 @@ async def category(message: types.Message):
 @dp.message_handler(Text(equals=['View sale']))
 @dp.message_handler(Text(equals=['Smartphones', 'Tablets', 'Laptops', 'Televisions', 'Refrigerators', 'Washing machines', 'View sale']))
 async def parsing(message: types.Message):
-	data_category = {'Smartphones': '205', 'Tablets': '195', 'Laptops': '118', 'Televisions': '1',
-	                 'Refrigerators': '159', 'Washing machines': '89'}
-	start_buttons = [*sales_numbers, 'Back 🔙']
+	data_category = data_base_category
+	start_buttons = [*sales_numbers, 'Back']
 	keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 	keyboard.add(*start_buttons)
 	if message.text != 'View sale':
@@ -48,11 +48,9 @@ async def parsing(message: types.Message):
 		except FileNotFoundError:
 			pass
 		get_data(data_category[message.text], message.from_user.first_name)
-		print('[!]Parsing complete')
-		await message.answer('Complete')
+		print('[INFO]Parsing complete')
 		await message.answer('Enter the tracked discount', reply_markup=keyboard, )
 	else:
-		await message.answer('Complete')
 		await message.answer('Enter the tracked discount', reply_markup=keyboard, )
 
 
@@ -80,7 +78,7 @@ async def search_sale(message: types.Message):
 			       f'Sale: {val["item_sale"]} %\n\n'
 			await message.answer(card)
 			cnt += 1
-			time.sleep(1)
+			time.sleep(3)
 	if cnt == 0:
 		await message.answer('No items')
 	else:
